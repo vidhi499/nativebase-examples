29dd16d78d14010d32c5d13c5a98e4da7314f14d
Vidhi Kataria <vidhi@Vidhi499.local>
2020-12-29 13:57:57 +0530

    nativebase working

--- uber-clone/App.tsx
+++ uber-clone/App.tsx
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Box, NativeBaseProvider } from "native-base";
+import { Flex } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Flex flex={1} bg="white">
+        <Box>yfyjfjyuf</Box>
+      </Flex>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
