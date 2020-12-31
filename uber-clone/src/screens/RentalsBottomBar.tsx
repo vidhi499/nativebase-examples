f59592ca224e5c5e671f2e9cf135c3f69ddfc89d
Vidhi Kataria <vidhi@Vidhi499.local>
2020-12-31 13:31:50 +0530

    rentals page

--- uber-clone/src/screens/RentalsBottomBar.tsx
+++ uber-clone/src/screens/RentalsBottomBar.tsx
@@ -17,26 +17,86 @@ import {
 export default function RentalsBottomBar() {
   return (
     <Box width="100%" bg="white" zIndex={2} bottom={0} position="absolute">
-      <Box bg="white" p={5}>
+      <Box bg="white" py={5}>
         <VStack space={3}>
           <Row justifyContent="center">
             <Text fontSize={16} fontWeight={400}>
               Choose a ride, swipe up for more
             </Text>
           </Row>
-          <Divider borderWidth={0.35} borderColor="gray.400" />
-          <Row bg="white" p={2}>
-            <Icon viewBox="0 0 512 512" color="gray.500" size={8}>
-              <Path d="M341.476,338.285c54.483-85.493,47.634-74.827,49.204-77.056C410.516,233.251,421,200.322,421,166    C421,74.98,347.139,0,256,0C165.158,0,91,74.832,91,166c0,34.3,10.704,68.091,31.19,96.446l48.332,75.84    C118.847,346.227,31,369.892,31,422c0,18.995,12.398,46.065,71.462,67.159C143.704,503.888,198.231,512,256,512    c108.025,0,225-30.472,225-90C481,369.883,393.256,346.243,341.476,338.285z M147.249,245.945    c-0.165-0.258-0.337-0.51-0.517-0.758C129.685,221.735,121,193.941,121,166c0-75.018,60.406-136,135-136    c74.439,0,135,61.009,135,136c0,27.986-8.521,54.837-24.646,77.671c-1.445,1.906,6.094-9.806-110.354,172.918L147.249,245.945z     M256,482c-117.994,0-195-34.683-195-60c0-17.016,39.568-44.995,127.248-55.901l55.102,86.463    c2.754,4.322,7.524,6.938,12.649,6.938s9.896-2.617,12.649-6.938l55.101-86.463C411.431,377.005,451,404.984,451,422    C451,447.102,374.687,482,256,482z" />
-              <Path d="M256,91c-41.355,0-75,33.645-75,75s33.645,75,75,75c41.355,0,75-33.645,75-75S297.355,91,256,91z M256,211    c-24.813,0-45-20.187-45-45s20.187-45,45-45s45,20.187,45,45S280.813,211,256,211z" />
-            </Icon>
 
-            <Box pl={4} pt={1}>
-              <Text fontWeight={600} fontSize={18}>
-                Set location on map
-              </Text>
-            </Box>
+          <Row bg="primary" px={4} py={4}>
+            <Image
+              alt="car-image"
+              size={16}
+              source={{
+                uri:
+                  "https://d3i4yxtzktqr9n.cloudfront.net/hulk/c4ca70eada901c083e41a6c10adcdc3b.png",
+              }}
+            />
+            <VStack>
+              <HStack space={5} pl={3} pt={2}>
+                <Text fontWeight={500} fontSize={20}>
+                  UberGo Rentals
+                </Text>
+                <HStack space={2}>
+                  <Icon viewBox="0 0 24 24" color="black" mt={1} size={4}>
+                    <Path d="M20.822 18.096c-3.439-.794-6.641-1.49-5.09-4.418 4.719-8.912 1.251-13.678-3.732-13.678-5.081 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-2.979.688-3.178 2.143-3.178 4.663l.005 1.241h10.483l.704-3h1.615l.704 3h10.483l.005-1.241c.001-2.52-.198-3.975-3.177-4.663zm-8.231 1.904h-1.164l-.91-2h2.994l-.92 2z" />
+                  </Icon>
+                  <Text fontSize={18} fontWeight={500} mt="2px">
+                    4
+                  </Text>
+                </HStack>
+                <Box ml="2"></Box>
+                <Text fontWeight={500} fontSize={20}>
+                  $159.00
+                </Text>
+              </HStack>
+              <Row space={5} pl={3} pt={2} justifyContent="space-between">
+                <Text fontWeight={400} fontSize={16}>
+                  11:32 am pickup
+                </Text>
+
+                <Box ml="2"></Box>
+                <Text fontWeight={400} color="gray.500" fontSize={16}>
+                  1 hr/10 km
+                </Text>
+              </Row>
+            </VStack>
           </Row>
+          <Row bg="white" px={6}>
+            <Image
+              alt="car-image"
+              size={12}
+              source={require("../../assets/cash.png")}
+            />
+            <Row
+              justifyContent="space-between"
+              width="90%"
+              space={5}
+              pl={5}
+              pt={2}
+            >
+              <Text fontWeight={500} fontSize={20}>
+                Cash
+              </Text>
+              <Icon viewBox="0 0 16 16" mt={1} size={5} color="black">
+                <Path d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
+              </Icon>
+            </Row>
+          </Row>
+          <Box px={4} mt={2}>
+            <Button
+              color="white"
+              p={4}
+              fontSize={18}
+              borderRadius={0}
+              variant="solid"
+              bg="black"
+            >
+              Confirm UberGo Rentals
+            </Button>
+          </Box>
         </VStack>
       </Box>
     </Box>
