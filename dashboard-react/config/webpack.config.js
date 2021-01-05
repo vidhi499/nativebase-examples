a5fc84c299c15dc1cdb61044e228f46f234e0fbe
Meenu Makkar <meenumakkar@Meenus-MacBook-Pro.local>
2021-01-05 14:29:50 +0530

    changes in dashboard reacy

--- dashboard-react/config/webpack.config.js
+++ dashboard-react/config/webpack.config.js
@@ -451,7 +451,7 @@ module.exports = function (webpackEnv) {
               loader: require.resolve("babel-loader"),
               options: {
                 babelrc: false,
-                configFile: false,
+                configFile: false,''
                 compact: false,
                 presets: [
                   [
